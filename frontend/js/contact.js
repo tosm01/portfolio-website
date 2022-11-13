bc74ad2d7661d0c16f64bd3cf1205f37596d430d
tosm01 <taha2013.to@gmail.com>
2022-11-13 20:41:17 +0800

    Implemented contact form

---
+++ frontend/js/contact.js
@@ -0,0 +1,31 @@
+async function submitContactForm() {
+    sender_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    subject = document.getElementById("subject").value;
+    message = document.getElementById("message").value;
+
+    body = `${message}\nName: ${sender_name}\nEmail: ${email}`;
+
+    let url = 'http://127.0.0.1:8000/api/v1/email';
+    let data = { "subject": subject, "body": body, "recipient": "tahaozairo@gmail.com", "sender": "PortfolioContact@12c6ff0e-b23f-4a55-8168-68b650da85cd.azurecomm.net" };
+
+    try {
+        let res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (res.ok) {
+            alert("Email sent");
+        } else {
+            alert("Error: Sending the Email failed. Please try again.");
+        }
+    } catch (error) {
+        alert("Error: Sending the Email failed. Please try again.");
+    }
+}
+
+document.getElementById("submitButton").addEventListener("click", submitContactForm);
